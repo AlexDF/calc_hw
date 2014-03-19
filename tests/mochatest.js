f6e01017b3e386842de276f65fc630ec22dbcd46
Alex DeFreitas <abd9@njit.edu>
2014-03-19 04:03:15 -0400

    added test for add function from calcfunctions.js file

--- tests/mochatest.js
+++ tests/mochatest.js
@@ -3,6 +3,13 @@ var funcs = require('../calcfunctions.js');
 
 
 describe("Calculator Functions", function() {
+  describe("addition", function() {
+    it("should return the sum of two numbers", function() {
+      var num1 = 2;
+      var num2 = 3;
+      var sum = funcs.add(num1, num2);
 
-
+      expect(sum).to.equal(5);
+    });
+  });
 });
