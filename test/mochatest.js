21ce030f3dc036fdd66da45fc743101c1c1918f7
Alex DeFreitas <abd9@njit.edu>
2014-03-19 05:31:10 -0400

    made minor change - not important

--- test/mochatest.js
+++ test/mochatest.js
@@ -1,13 +1,14 @@
 var expect = require("chai").expect;
-var funcs = require('../calcfunctions.js');
+var funcs = require("../lib/calcfunctionsmocha.js");
 
 
 describe("Calculator Functions", function() {
-  describe("addition", function() {
+  describe("#add()", function() {
     it("should return the sum of two numbers", function() {
-      var num1 = 2;
-      var num2 = 3;
-      var sum = funcs.add(num1, num2);
+      var n1 = 2;
+      var n2 = 3;
+      var sum = funcs.add(n1, n2);
+
 
       expect(sum).to.equal(5);
     });
