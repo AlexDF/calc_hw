6d7c013f065967f296ff318b0536c3d032015ec7
Alex DeFreitas <abd9@njit.edu>
2014-03-21 18:16:38 -0400

    Phase 1/Part 1 complete - calc app can add two integers and readout behaves like a calculator

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
+var total = "0";
+var additionFlag = 1;
+var startOver = 0;
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express', digits: '0' });
 };
@@ -10,11 +14,37 @@ exports.index = function(req, res){
 exports.updateReadout = function(req, res) {
   var readoutVal = req.query.readoutVal;
   var digit = req.query.digit;
+  var operation = req.query.operation;
 
-  if( readoutVal == "0" ) {
-    res.render('index', { digits: digit});
-  } else {
-    readoutVal = readoutVal + digit;
-    res.render('index', { digits: readoutVal});
+if( operation !== "0" ) {
+  if( additionFlag === 1 && operation !== "equals" ) {
+    total = ( parseInt(total) + parseInt(readoutVal) ).toString();
+    additionFlag = 0;
+  }
+  
+  if( operation === "plus" ) {
+    additionFlag = 1;
+  } else if( operation === "equals" ) {
+    total = ( parseInt(total) + parseInt(readoutVal) ).toString();
+    res.render('index', {digits: total});
+    total = "0"; 
   }
 }
+  
+  if( operation === "equals" || operation === "0" ) {
+    if( readoutVal === "0" ||  startOver === 1 ) {
+      res.render('index', {digits: digit});
+    } else {
+      readoutVal = readoutVal + digit;
+      res.render('index', {digits: readoutVal});
+    }
+  }
+
+if( operation !== "0" ) {
+  startOver = 1;
+} else {
+  startOver = 0;
+}
+
+}
+
