3fd664f8ebaa3bd2ca8185779f15341d273bc5ec
Alex DeFreitas <abd9@njit.edu>
2014-03-26 15:19:03 -0400

    fixed front-end unit tests to reflect the actual code used in app to perform calculations

--- calcfunctions.js
+++ calcfunctions.js
@@ -1,18 +1,18 @@
 function add(num1, num2) {
-  return num1 + num2;
+  return ( parseFloat(num1) + parseFloat(num2) ).toFixed(5).toString();
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return ( parseFloat(num1) - parseFloat(num2) ).toFixed(5).toString();
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return ( parseFloat(num1) * parseFloat(num2) ).toFixed(5).toString();
 }
 
 function divide(num1, num2) {
-  if( num2 !== 0 ) {
-    return num1 / num2;
+  if( num2 !== "0" ) {
+    return ( parseFloat(num1) / parseFloat(num2) ).toFixed(5).toString();
   }
   else {
     return "ERROR";
