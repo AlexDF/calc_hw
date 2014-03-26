1da75497df63eca9dc732b63a8d8dd27400b8c0a
Alex DeFreitas <abd9@njit.edu>
2014-03-26 15:33:23 -0400

    changed mocha unit tests to reflect code actually used to perform the arithmetic calculations

--- test/mochatest.js
+++ test/mochatest.js
@@ -5,12 +5,44 @@ var funcs = require("../lib/calcfunctionsmocha.js");
 describe("Calculator Functions", function() {
   describe("#add()", function() {
     it("should return the sum of two numbers", function() {
-      var n1 = 2;
-      var n2 = 3;
+      var n1 = "2";
+      var n2 = "3";
       var sum = funcs.add(n1, n2);
 
-
-      expect(sum).to.equal(5);
+      expect(sum).to.equal("5.00000");
     });
   });
+
+  describe("#subtract()", function() {
+    it("should return the difference of two numbers", function() {
+      var n1 = "2";
+      var n2 = "3";
+      var diff = funcs.subtract(n1, n2);
+
+      expect(diff).to.equal("-1.00000");
+    });
+  });
+
+  describe("#subtract()", function() {
+    it("should return the product of two numbers", function() {
+      var n1 = "2";
+      var n2 = "3";
+      var product = funcs.multiply(n1, n2);
+
+      expect(product).to.equal("6.00000");
+    });
+  });
+
+  describe("#divide()", function() {
+    it("should return the quotient of two numbers", function() {
+      var n1 = "6";
+      var n2 = "3";
+      var quotient = funcs.divide(n1, n2);
+
+      expect(quotient).to.equal("2.00000");
+    });
+  });
+
+
+
 });
